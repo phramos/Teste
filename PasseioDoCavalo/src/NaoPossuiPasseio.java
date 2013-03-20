df6a19646ccb732109df965bbef4d606f6cae806
pedro <pedro.lemaf@gmail.com>
2013-03-20 14:35:13 -0300

    Atualização dos arquivos do passeio do cavalo

--- PasseioDoCavalo/src/NaoPossuiPasseio.java
+++
@@ -1,6 +0,0 @@
-//Exceção lançada caso não exista passeio no tabuleiro com a posição inicial informada
-public class NaoPossuiPasseio extends RuntimeException {
-	public NaoPossuiPasseio(){
-		super("Não possui passeio do cavalo com as coordenadas iniciais informadas");
-	}
-}
